ced016b014eefd90ecbe93180f9d41389c69d2c3
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-25 10:02:30 +0200

    searchbar /marketplace to find for a specific game done

--- src/pages/MarketplacePage.js
+++ src/pages/MarketplacePage.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Navbar from "../components/navbar/Navbar";
 import { Link } from "react-router-dom";
 import { withAuth } from "../lib/AuthProvider";
 import AddGame from "../components/addgames/AddGame";
+import SearchBar from "../components/searchbar/SearchBar";
 import axios from "axios";
 
 import Card from "react-bootstrap/Card";
@@ -10,7 +10,8 @@ import Button from "react-bootstrap/Button";
 
 class MarketplacePage extends Component {
   state = {
-    offersToShow: [],
+    allOffers: [], //All offers
+    offersToShow: [], //What we will see with the search bar
   };
 
   componentDidMount() {
@@ -18,25 +19,48 @@ class MarketplacePage extends Component {
       .get("http://localhost:4000/api/offer")
       .then((allOffers) => {
         console.log(allOffers.data);
-        this.setState({ offersToShow: allOffers.data });
+        this.setState({
+          allOffers: allOffers.data,
+          offersToShow: allOffers.data,
+        });
       })
       .catch((err) => console.log(err));
   }
 
-  componentDidUpdate() {
-    console.log(this.state);
-  }
+  filterGames = (searchString) => {
+    const lowerSearchString = searchString.toLowerCase();
+
+    const gamesCopy = [...this.state.allOffers];
+    const filteredGames = gamesCopy.filter((gameObj) => {
+      console.log(gameObj);
+      const gameName = gameObj.videoGameName.toLowerCase();
+
+      if (gameName.includes(lowerSearchString)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    this.setState({ offersToShow: filteredGames });
+  };
 
   render() {
     const { offersToShow } = this.state;
     return (
       <div>
         <h1>MARKETPLACE</h1>
-        <p>Sell your games</p>
+        <p>Buy a game</p>
+
+        <div className="header" id="myHeader">
+          {/* SEARCH BAR */}
+          <SearchBar filterGames={this.filterGames} />
+        </div>
+
         {offersToShow.map((offer) => {
           return (
-            <div id="container-all-offers">
-              <main id="all-offers">
+            <div className="container-all-offers">
+              <main className="all-offers">
                 <Card key={offer._id}>
                   <Card.Img
                     variant="top"
@@ -46,26 +70,16 @@ class MarketplacePage extends Component {
                   <Card.Body>
                     <Card.Title>{offer.videoGameName}</Card.Title>
                     <p>{offer.childrenPlatform}</p>
-                    <p>{offer.price}</p>
+                    <p>{offer.price} €</p>
                     <Link to={`/videogames/${offer.videoGameId}`}>
-                      <Button variant="danger">Sell this game</Button>
+                      <Button variant="danger">
+                        Details <small>about the game</small>
+                      </Button>
                     </Link>
                   </Card.Body>
                 </Card>
               </main>
             </div>
-            /* <div className="offersToShow" key={offer._id}>
-              <img
-                className="img-games-marketplace"
-                src={offer.videoGamePic}
-                alt={offer.videoGameName}
-              />
-              <p>{offer.videoGameName}</p>
-              <p>{offer.childrenPlatform}</p>
-              <p>{offer.price}</p>
-
-              <Link to={`/videogames/${offer.videoGameId}`}>Details</Link>
-            </div> */
           );
         })}
         <AddGame />

5943ecb70abac78a4271c0ec18cd21974f3872ea
Noiret <sergio.ko@icloud.com>
2020-08-27 13:22:50 +0200

    Deploy 3.2: updated readme and styling

--- build/precache-manifest.8206a4d3621b6c23e7720f33c0935ab5.js
+++ build/precache-manifest.8206a4d3621b6c23e7720f33c0935ab5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99de6a6a24377a85cb5d008b986b6086",
+    "revision": "0caa3dfc738d4eb7c764b7f69adc5cd6",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f3cffc9e.chunk.css"
   },
   {
-    "revision": "5587b016db42ea666e72",
-    "url": "/static/css/main.3d0acb34.chunk.css"
+    "revision": "f70f1f0034254fe92b7f",
+    "url": "/static/css/main.c2aca0c0.chunk.css"
   },
   {
     "revision": "11f2741d302c73cc7338",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b9ef0ada.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5587b016db42ea666e72",
-    "url": "/static/js/main.9b757b4c.chunk.js"
+    "revision": "f70f1f0034254fe92b7f",
+    "url": "/static/js/main.5cd6bf37.chunk.js"
   },
   {
     "revision": "5179ff928fc7dafdbfe9",

19623066bf0488191fae36af0d5c199e031f73bf
Noiret <sergio.ko@icloud.com>
2020-10-05 15:57:13 +0200

    Deploy en Heroku del fix del profile

--- src/components/AnonRoute.js
+++ src/components/AnonRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { withAuth } from "../lib/AuthProvider";
 
-// El componente <AnonRoute /> recibe como argumento un objecto con las propiedades: component con el valor de un componente (Signup, Login), isLoggedin (viene de withAuth, ya que Signup se exporta como withAuth(Signup), y el resto de las props (si hubiera))
+//El componente <AnonRoute /> recibe como argumento un objecto con las propiedades: component con el valor de un componente (Signup, Login), isLoggedin (viene de withAuth, ya que Signup se exporta como withAuth(Signup), y el resto de las props (si hubiera))
 
 function AnonRoute({ component: Component, isLoggedin, ...rest }) {
   // devuelve un componente <Route /> donde su prop render recibe las props, y si no está logueado, devuelve el componente con sus props (history, etc.), en caso contrario, el componente <Redirect /> redirige a /private
